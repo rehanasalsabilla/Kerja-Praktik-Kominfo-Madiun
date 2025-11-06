4c0c7e02dcf7b3f8bcdbc7d33240541a566f84da
Rehana <rehanaputri814@gmail.com>
2025-11-06 22:47:08 +0700

    Fixed 7

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,14 @@
+// resources/js/app.js
 import '../css/app.css';
 import './bootstrap';
 
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import route from 'ziggy-js'; // import Ziggy NPM
+
+// Buat route global agar bisa dipakai di semua komponen
+window.route = route;
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -13,12 +17,11 @@ createInertiaApp({
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.vue`,
-            import.meta.glob('./Pages/**/*.vue'),
+            import.meta.glob('./Pages/**/*.vue')
         ),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(ZiggyVue)
             .mount(el);
     },
     progress: {
